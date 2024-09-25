cf216413b9a58ee2614c8d7adb7b63da7d64f205
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2024-09-25 11:42:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+  public static void main(String args[]){
+  RWOnly r = new RWOnly();
+    r.setName("srikanth");
+    r.setAge(10);
+    r.setAddress("guntur");
+    System.out.println(r.getName());
+    System.out.println(r.getAge());
+    System.out.println(r.getAddress());
+  }
+
   
 }
